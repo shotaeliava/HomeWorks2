1fe76cfb4ae6389d173efee8c073b57e1cf3f502
shotaeliava <shota.eliava@gmail.com>
2022-04-26 23:10:38 +0400

    visual studio solution

---
+++ solution/solution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("My name is Shota Eliava");
+            Console.WriteLine("What Yours?");
+            string Texst = Console.ReadLine();
+            Console.WriteLine("Your?" + Texst);
+        }
+    }
+}
