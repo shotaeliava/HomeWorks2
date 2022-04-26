40d306a91f7160f2cbc31e5ef84506ea182d6a39
shotaeliava <shota.eliava@gmail.com>
2022-04-26 21:37:23 +0400

    HomeWork2

---
+++ shotaeliava2/shotaeliava2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace shotaeliava2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("My name is Shota Eliava");
+            Console.WriteLine("What Yours?");
+            string Texst=Console.ReadLine();
+            Console.WriteLine("What Yours?" + Texst);
+        }
+    }
+}
